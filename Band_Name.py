425a59dad31875eff549cb3ac8c5e8710bf21362
uday9989 <91973463+uday9989@users.noreply.github.com>
2024-12-01 13:19:10 +0000

    Add files via upload

---
+++ Band_Name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name =('welcome to the band name generator')
+print(name)
+city = input('what is the name of  the  city you grew up  in ')
+print(city)
+pet_Name =input('what is  your  pet  name ')
+print(pet_Name)
+Band_name = (city +'' + pet_Name)
+print(Band_name)
+
+
+# In[ ]:
+
+
+
+
